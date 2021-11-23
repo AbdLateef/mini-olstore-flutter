ab14071ed9eea0114afaa968b6f1ce17d2e7ce08
Abdul Lathif <lathif.is10@gmail.com>
2021-11-23 22:32:22 +0700

    Feat: Grid Layout - Product

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -9,6 +9,7 @@ class Products extends StatelessWidget {
   }) : super(key: key);
 
   final String url = 'https://fakestoreapi.com/products';
+  // android emulator http://10.0.2.2
 
   Future getProducts() async {
     var response = await http.get(Uri.parse(url));
@@ -23,9 +24,70 @@ class Products extends StatelessWidget {
       appBar: AppBar(
         title: Text('Mini Store'),
       ),
-      body: Container(
-        child: Text('test'),
-      ),
+      // body: FutureBuilder(
+      //   future: getProducts(),
+      //   builder:(context, AsyncSnapshot snapshot) {
+      //     if(snapshot.hasData) {
+      //       return ListView.builder(
+      //         itemCount: snapshot.data.length,
+      //         itemBuilder: (context, index) {
+      //           return Text(snapshot.data[index]['title']); 
+      //         });
+      //     } else {
+      //       return Center(child: CircularProgressIndicator());
+      //     }
+      //   }
+      // )
+      body: FutureBuilder(
+        future: getProducts(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return GridView.builder(
+                itemCount: snapshot.data.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2
+                ),
+                itemBuilder: (context, index) {
+                  return Container(
+                    //height: 180,
+                    child: Card(
+                      elevation: 5,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 120,
+                            padding: EdgeInsets.all(5),
+                            child: Image.network(snapshot.data[index]['image'])
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: Text(snapshot.data[index]['title'])
+                          ),
+                          Row(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: Text('Rp XXXX')
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: Text('Buy Button')
+                              ),  
+                            ],
+                          )
+                        ],
+                      )
+                    )
+                  );
+                  //return Text(snapshot.data[index]['title']);
+                }
+            );
+          } else if (snapshot.hasError) {
+            return Text("Error");
+          }
+          return Text("Loading...");
+        },
+      )  
     );
   }
 }
