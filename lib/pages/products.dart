75748d76b8dc9b6bafa99a5dd40ce916b120359d
Lathif Jumatiawan <lathif.is10@gmail.com>
2021-11-24 19:22:04 +0700

    tes api

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -9,8 +9,9 @@ class Products extends StatelessWidget {
   }) : super(key: key);
 
   final String url = 'https://fakestoreapi.com/products';
-  // final String url = 'http://10.0.2.2/products';
-  // android emulator http://10.0.2.2
+  // final String url = 'http://127.0.0.1:8000/products';
+  // android emulator https://10.0.2.2
+  // final String url = 'https://10.0.2.2:8000/products';
 
   Future getProducts() async {
     var response = await http.get(Uri.parse(url));
@@ -25,70 +26,70 @@ class Products extends StatelessWidget {
       appBar: AppBar(
         title: Text('Mini Store'),
       ),
-      // body: FutureBuilder(
-      //   future: getProducts(),
-      //   builder:(context, AsyncSnapshot snapshot) {
-      //     if(snapshot.hasData) {
-      //       return ListView.builder(
-      //         itemCount: snapshot.data.length,
-      //         itemBuilder: (context, index) {
-      //           return Text(snapshot.data[index]['title']); 
-      //         });
-      //     } else {
-      //       return Center(child: CircularProgressIndicator());
-      //     }
-      //   }
-      // )
       body: FutureBuilder(
         future: getProducts(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            return GridView.builder(
-                itemCount: snapshot.data.length,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2
-                ),
-                itemBuilder: (context, index) {
-                  return Container(
-                    //height: 180,
-                    child: Card(
-                      elevation: 5,
-                      child: Column(
-                        children: [
-                          Container(
-                            height: 120,
-                            padding: EdgeInsets.all(5),
-                            child: Image.network(snapshot.data[index]['image_url'])
-                          ),
-                          Container(
-                            padding: EdgeInsets.all(10),
-                            child: Text(snapshot.data[index]['title'])
-                          ),
-                          Row(
-                            children: [
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text('Rp XXXX')
-                              ),
-                              Container(
-                                padding: EdgeInsets.all(10),
-                                child: Text('Buy Button')
-                              ),  
-                            ],
-                          )
-                        ],
-                      )
-                    )
-                  );
-                  //return Text(snapshot.data[index]['title']);
-                }
-            );
-          } else if (snapshot.hasError) {
-            return Text("Error");
+        builder:(context, AsyncSnapshot snapshot) {
+          if(snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                return Text(snapshot.data[index]['title']); 
+              });
+          } else {
+            return Center(child: CircularProgressIndicator());
           }
-          return Text("Loading...");
-        },
-      )  
+        }
+      )
+      // body: FutureBuilder(
+      //   future: getProducts(),
+      //   builder: (context, AsyncSnapshot snapshot) {
+      //     if (snapshot.hasData) {
+      //       return GridView.builder(
+      //           itemCount: snapshot.data.length,
+      //           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //             crossAxisCount: 2
+      //           ),
+      //           itemBuilder: (context, index) {
+      //             return Container(
+      //               //height: 180,
+      //               child: Card(
+      //                 elevation: 5,
+      //                 child: Column(
+      //                   children: [
+      //                     Container(
+      //                       height: 120,
+      //                       padding: EdgeInsets.all(5),
+      //                       child: Image.network(snapshot.data[index]['image_url'])
+      //                     ),
+      //                     Container(
+      //                       padding: EdgeInsets.all(10),
+      //                       child: Text(snapshot.data[index]['title'])
+      //                     ),
+      //                     Row(
+      //                       children: [
+      //                         Container(
+      //                           padding: EdgeInsets.all(10),
+      //                           child: Text('Rp XXXX')
+      //                         ),
+      //                         Container(
+      //                           padding: EdgeInsets.all(10),
+      //                           child: Text('Buy Button')
+      //                         ),  
+      //                       ],
+      //                     )
+      //                   ],
+      //                 )
+      //               )
+      //             );
+      //             //return Text(snapshot.data[index]['title']);
+      //           }
+      //       );
+      //     } else if (snapshot.hasError) {
+      //       return Text("Error");
+      //     }
+      //     return Text("Loading...");
+      //   },
+      // )  
     );
   }
 }
